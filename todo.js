d9c31b34888e1cf2e9a6d78b4754ba01138fb92a
hackthissite0214 <54093808+hackthissite0214@users.noreply.github.com>
2020-09-07 22:09:20 +0900

    Update todo.js

--- todo.js
+++ todo.js
@@ -4,15 +4,61 @@ const toDoForm = document.querySelector('.js-toDoForm'),
 
 const TODOS_LS = 'toDos';
 
+/*
+function filterFn(toDo) {
+    // toDo의 id가 1인 경우만 리턴
+    return toDo.id === 1
+}
+*/
+
+let toDos = [];
+
+function deleteToDo(event) {
+    //  어떤 버튼이 클릭되었는지
+    //  버튼의 부모가 누구인지
+    // console.log(event.target.parentNode);
+
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+
+    // filter는 마치 forEach에서 function을 실행하는 것 같이 각각의 item과 같이 실행
+    // filter는 array의 모든 아이템을 통해 함수를 실행하고 
+    // 그리고 true인 아이템들만 가지고 새로운 array를 만들고 
+    //  const cleanToDos = toDos.filter(filterFn);
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+    });
+
+    //console.log(cleanToDos);
+    toDos = cleanToDos;
+    saveToDos();
+}
+
+function saveToDos() {
+    // localStorage는 무조건 string만 저장
+    // 자바스크립트 object를 string 바꿔준다
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
    const li = document.createElement('li');
    const delBtn = document.createElement('button');
-   delBtn.innerText = "❌";
    const span = document.createElement('span');
+   const newId = toDos.length + 1;
+   delBtn.innerText = "❌";
+   delBtn.addEventListener('click', deleteToDo);
    span.innerText = text;
    li.appendChild(delBtn);
    li.appendChild(span);
+   li.id = newId;
    toDoList.appendChild(li);
+   const toDoObj = {
+       text: text,
+       id: newId
+   }
+   toDos.push(toDoObj);
+   saveToDos();
 }
 
 function handleSubmit(event) {
@@ -23,9 +69,14 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null) {
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null) {
+        //  불러온것은 json으로 변환된 object가 아닌 string이다.
+        //  다시 object로 변환
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text);
+        })
     }
 }
 
